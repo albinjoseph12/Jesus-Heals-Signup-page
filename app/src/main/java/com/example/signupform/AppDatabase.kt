7442251ff42b1e29382bcaa2dc76d0848fe56b90
albinjoseph12 <31781931+albinjoseph12@users.noreply.github.com>
2024-08-12 13:15:54 -0700

    Deleted all files

--- app/src/main/java/com/example/signupform/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.signupform
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Registration::class], version = 1, exportSchema = false)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun registrationDao(): RegistrationDao
-}
